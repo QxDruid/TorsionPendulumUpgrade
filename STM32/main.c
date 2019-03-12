70c5f03d8ffb31a5ac5a911ab1bfc448b579c56e
QxDruid <QxDruid@users.noreply.github.com>
2019-03-12 22:35:47 +0700

    reciev test command from USART added

--- STM32/main.c
+++ STM32/main.c
@@ -1,17 +1,68 @@
 #include "init.h"
 
+volatile uint8_t data_ready_flag = 0;
+volatile uint8_t CCD_read_flag = 0;
+
 // Буфер чения линейки
 volatile uint16_t CCD_Buffer[3694]; 
-
+volatile uint8_t RX_data = '\0';
+char USART_buffer[50] = {'\0'};
+    
 int main()
 {
     // Настройка такирование ядра и переферии
     sysclock_init();
     gpio_init();
     timer_init();
+    adc_init();
+    usart_init();    
+    nvic_init();
+    
+    USARTSend("0\n", sizeof("0\n"));
+    USARTSend("CCD Ready\n", sizeof("CCD Ready\n"));
+    
     
     while(1)
     {
+        if(RX_data != '\0')
+        {
+                        
+            sprintf(USART_buffer, "data is - %c\r\n", RX_data);
+            USARTSend(USART_buffer, sizeof(USART_buffer)); 
+            
+            if(RX_data == 'g')
+            {    
+                CCD_read_flag = 1;
+                GPIOC->ODR ^= GPIO_Pin_13;   
+            }
+            if(RX_data == 't')
+            {    
+                for(int i = 0; i < 3694; i++)
+                {
+            // Записываем значение int в строку вывода как массив char
+                    sprintf(USART_buffer, "%d\n", CCD_Buffer[i]);
+                // Отправляем данные
+                    USARTSend(USART_buffer, sizeof(USART_buffer));
+                }  
+            }
+            RX_data = '\0';
+        }
+
+        if(data_ready_flag == 1)
+        {
+
+            for(int i = 0; i < 3694; i++)
+            {
+            // Записываем значение int в строку вывода как массив char
+                sprintf(USART_buffer, "%d\n", CCD_Buffer[i]);
+                // Отправляем данные
+                USARTSend(USART_buffer, sizeof(USART_buffer));
+            }
+
+        }
+            
     }
     return 0;
-}
+    
+}    
+    
